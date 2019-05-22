a4007c78284531600fb758f3f47c93a457523cb0
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-22 15:23:17 +0700

    Using partials

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsDirectoryPath = path.join(__dirname, '../templates/views');
+const partialsDirectoryPath = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsDirectoryPath);
+hbs.registerPartials(partialsDirectoryPath);
+
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -19,7 +25,7 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/help', (req, res) => {
-	res.render('help', { title: 'Help', message: 'How can we help you today?' });
+	res.render('help', { title: 'Help', message: 'How can we help you today?', name: 'Peter Nguyen' });
 });
 app.get('/weather', (req, res) => {
 	res.send({ location: 'Philadelphia', weather: '35 degrees' });
