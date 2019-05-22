9aae0f088ce2d7454e8ff40065a468f7e9c9d8ab
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-22 14:29:55 +0700

    basic express server setup

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get('', (req, res) => {
+	res.send('Hello express!');
+});
+
+app.get('/help', (req, res) => {
+	res.send('What can we help you with?');
+});
+
+app.get('/about', (req, res) => {
+	res.send('About');
+});
+
+app.get('/weather', (req, res) => {
+	res.send('Weather');
+});
+app.listen(3000, () => {
+	console.log('Server is up on port 3000.');
+});
