f0edb9db352ee7e0c5b28c06cbc52ab22912f5e4
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-22 15:06:32 +0700

    convert html files to handlebar

--- src/app.js
+++ src/app.js
@@ -3,8 +3,24 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Peter Nguyen',
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', { title: 'About', name: 'Peter Nguyen' });
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', { title: 'Help', message: 'How can we help you today?' });
+});
 app.get('/weather', (req, res) => {
 	res.send({ location: 'Philadelphia', weather: '35 degrees' });
 });
