0ece5acf4bb394d574057c78d2a33a195e273f35
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-23 16:15:51 +0700

    connecting front end with backend request

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('JS file loaded');
+
+fetch('http://localhost:3000/weather?address=boston')
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		if (data.error) {
+			return console.log(data.error);
+		}
+
+		console.log(data);
+	});
