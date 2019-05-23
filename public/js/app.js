f02eaf4d2a0d8e09b238cf18a8e5f7527e9837ed
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-23 16:35:27 +0700

    add form to query weather backend

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,23 @@
 console.log('JS file loaded');
 
-fetch('http://localhost:3000/weather?address=boston')
-	.then(response => {
-		return response.json();
-	})
-	.then(data => {
-		if (data.error) {
-			return console.log(data.error);
-		}
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const weatherInfo = document.querySelector('#weather-info');
+const errorMessage = document.querySelector('#error');
+weatherForm.addEventListener('submit', event => {
+	event.preventDefault();
 
-		console.log(data);
-	});
+	const location = search.value;
+	weatherInfo.textContent = 'Loading...';
+
+	fetch(`http://localhost:3000/weather?address=${location}`)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			if (data.error) {
+				return (weatherInfo.textContent = data.error);
+			}
+			weatherInfo.textContent = data.weather.summary;
+		});
+});
