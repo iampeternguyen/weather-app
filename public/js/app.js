845781b3fee6c51ceda92ad988275aeaf67f4d66
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-23 16:44:57 +0700

    clean up and styling

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,14 @@ console.log('JS file loaded');
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const weatherInfo = document.querySelector('#weather-info');
-const errorMessage = document.querySelector('#error');
+const locationInfo = document.querySelector('#location');
+
 weatherForm.addEventListener('submit', event => {
 	event.preventDefault();
 
 	const location = search.value;
-	weatherInfo.textContent = 'Loading...';
+	locationInfo.textContent = '';
+	weatherInfo.textContent = 'Loading';
 
 	fetch(`http://localhost:3000/weather?address=${location}`)
 		.then(response => {
@@ -18,6 +20,7 @@ weatherForm.addEventListener('submit', event => {
 			if (data.error) {
 				return (weatherInfo.textContent = data.error);
 			}
+			locationInfo.textContent = data.location;
 			weatherInfo.textContent = data.weather.summary;
 		});
 });
